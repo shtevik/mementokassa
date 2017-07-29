d20c0e5ad4f724046e2a7d8b5c21b33417909420
shtevik <v.shtefanets+git@gmail.com>
2017-07-30 01:00:26 +0300

    Update mementokassa.js

--- mementokassa.js
+++ mementokassa.js
@@ -2,6 +2,7 @@
 // 0. check trigger phase - create / update
 var orderID = entry().field("orderID");
 var foundOrders = lib().find('"'+orderID+'"');
+message(foundOrders.length);
 if ( foundOrders.length == 0 ) {
 
 	message('create');

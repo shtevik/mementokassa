0694132e830a0584cd43769803a49bb384c98e18
shtevik <v.shtefanets+git@gmail.com>
2017-07-30 00:58:09 +0300

    Update mementokassa.js

--- mementokassa.js
+++ mementokassa.js
@@ -1,1 +1,13 @@
-message('hello git, really?');
+
+// 0. check trigger phase - create / update
+var orderID = entry().field("orderID");
+var foundOrders = lib().find('"'+orderID+'"');
+if ( foundOrders.length == 0 ) {
+
+	message('create');
+  
+else {
+  
+  message('update');
+  
+}

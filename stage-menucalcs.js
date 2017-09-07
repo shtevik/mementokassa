5eb2c8048b976c70bc941330c1a0035cb3cba053
shtevik <v.shtefanets+git@gmail.com>
2017-09-08 01:38:07 +0300

    Update stage-menucalcs.js

--- stage-menucalcs.js
+++ stage-menucalcs.js
@@ -3,20 +3,42 @@ var menu = entry().field('Меню');
 if ( menu.length > 0 ) {
 
   var structuredMenu = {};
+  var addStamps = 0;
 
-  for ( var position, i=0; i < menu.length; i++ ) {
+  for ( var position, price, i=0; i < menu.length; i++ ) {
   
     position = menu[i].field("Наименование");
+    price = menu[i].field("Цена");
+    
 
-    if ( structuredMenu[position] === null ) structuredMenu[position] = 1;
-    else structuredMenu[position]++;
+    if ( !structuredMenu[position] ) {
+      //message ('null');
+      structuredMenu[position].count = 1;
+      structuredMenu[position].price = price; 
+    } else { 
+      structuredMenu[position].count ++;
+      //message ('exsist');
+    }
     
-    message(structuredMenu[position]);
-    
-    
+    addStamps++;   
 
   }
   
+  
+  var x;
+  var totalSum = 0;
+  var orderString = '';
+  
+  for ( x in structuredMenu ) {
+    
+    totalSum += structuredMen[x].price * structuredMen[x].count;
+    orderString += ', '+ structuredMen[x].count +'x'+ x;
+    
+    
+  }
+  
+  message (total);
+  message(orderString);
   
 
 
